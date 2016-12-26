cc3eb247fed226a0d2394af2752e31d16b758b77
rizkydarmawan <rizkydarmawanrd16@gmail.com>
2016-12-26 22:40:11 +0700

    tambahan submit

--- data-siswa/input_siswa.php
+++ data-siswa/input_siswa.php
@@ -2,3 +2,4 @@
 		<input type="text" name="nama" id="nama" placeholder="Nama"><br>
 		<input type="text" name="usia" id="usia" placeholder="Usia"><br>
 		<input type="submit" name="submit" id="submit" value="Submit">
+		<input type="submit" name="submit" id="submit" value="Submit">
